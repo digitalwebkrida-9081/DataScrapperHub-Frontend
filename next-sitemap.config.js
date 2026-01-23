dbab3b64879eaa7ae444163c3c8d8cd659a8dd99
digitalwebkrida-9081 <digitalwebkrida@gmail.com>
2026-01-23 13:44:49 +0530

    Update dist folder and sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://datascraperhub.com', // Replace with your actual site URL
+  generateRobotsTxt: true, 
+  outDir: 'out',
+  // ...other options
+};
