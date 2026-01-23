9e0a638b43a8fe15c73545462945661d5cbf5335
ravikadia12 <ravmit12@gmail.com>
2026-01-23 14:29:48 +0530

    update dist

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://stag.datasellerhub.com', // 🔴 CHANGE THIS
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
