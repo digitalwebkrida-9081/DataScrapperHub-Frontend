37947167fd841a4061b06d5db85a4cda92dbd902
digitalwebkrida-9081 <digitalwebkrida@gmail.com>
2026-01-23 15:30:04 +0530

    Resolve merge conflict in next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-<<<<<<< HEAD
   siteUrl: 'https://datascraperhub.com', 
   generateRobotsTxt: true, 
   outDir: 'dist',
-
-=======
-  siteUrl: 'https://stagservice.datasellerhub.com', // 🔴 CHANGE THIS
-  generateRobotsTxt: true,
   sitemapSize: 7000,
->>>>>>> a19cb323bbdb17cb927307c25fe4af480406388a
 };
